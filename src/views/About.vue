0189cb07faa4be60997aa259729a210343470849
Jhoi <harrison.diaz@uptc.edu.co>
2022-03-21 11:06:08 -0500

    Se añadio el about y sus correspondientes componentes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,10 @@
 <template>
-<float-image img="https://scontent-bog1-1.xx.fbcdn.net/v/t39.30808-6/276320991_5272663946111051_7501200464921444384_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFXFTW3f1tMwPl4vOq2l1PIYR4FaPgU5d5hHgVo-BTl3vF_GmQDv-5P9IhIeDgDA7aHYjkwXH84yu1sme5421X1&_nc_ohc=aX2ZWM5veaUAX9647TM&tn=PLfXsc2n8YTrqzAI&_nc_ht=scontent-bog1-1.xx&oh=00_AT8Hz7m3X3OYuFrv0wlIL97lPIabW7AXZsA54HeeKPH8Gg&oe=623CD1EB" alt="JhoiSama" author="Harrison J. Diaz"
+<float-image img="https://i.pinimg.com/564x/90/0b/5c/900b5c0b9f15586f6a444cc8d1c6582f.jpg" alt="JhoiSama" author="Harrison J. Diaz"
 code="201920260" email="harrison.diaz@uptc.edu.co"></float-image>
-<float-image img="https://cn.i.cdn.ti-platform.com/content/483/showpage/foster-la-casa-de-los-amigos-imaginarios/es/fosters-200x200.png" alt="name" author="Autor"
-             code="codigo" email="correo"></float-image>
-<float-image></float-image>
+<float-image img="https://cn.i.cdn.ti-platform.com/content/483/showpage/foster-la-casa-de-los-amigos-imaginarios/es/fosters-200x200.png" alt="Julian Ardilla" author="Julian Ardila"
+             code="201929338" email="julian.ardila@uptc.edu.co"></float-image>
+<float-image img="https://i.pinimg.com/originals/43/44/08/434408f3232510def1fe3880555b8160.jpg" alt="Maria Fernanda Sanchez" author="Maria Fernanda Sanchez"
+             code="201521624" email="correo"></float-image>
 </template>
 
 <script>
